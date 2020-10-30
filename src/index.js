7d895a0732018520bceb831532739d3caee91fdd
Twixtaxe <46041463+Twixtaxe@users.noreply.github.com>
2020-10-30 04:35:40 -0300

    esqueci o cors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require("express")
 const routes = require("./routes")
+const cors = require("cors")
 
 require("dotenv").config()
 
 const app = express();
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(process.env.APP_PORT, () =>{
+app.listen(process.env.APP_PORT || 3000, () =>{
     return console.log("vai")
 })
 
